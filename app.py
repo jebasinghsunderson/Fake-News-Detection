d4d925b73726a1001d298736e827910e906e9541
jebasinghsundersoni <jebasinghsundersoni@gmail.com>
2026-06-07 21:11:38 +0530

    Search queries are generated in the form of json and returned from extractor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from urllib.parse import urlparse
+from modules.extractor import process_article
 
 st.title("Credibility of News Articles", text_alignment="center")
 st.write("This application evaluates the credibility of news articles using a machine learning model.")
@@ -10,5 +11,14 @@ if link:
     
     if parsed.scheme and parsed.netloc:
         st.success(f"Valid URL: {link}")
+        msg = st.empty()
+        msg.write("Processing the article...")
+        main_claim, result = process_article(link)
+        msg.write("Done!")
+        st.write("Extracted Information:")
+        st.write(f"Main Claim: {main_claim}")
+        st.write(f"Search Queries: {result}")
+
     else:
         st.error("Please enter a valid URL")
+
